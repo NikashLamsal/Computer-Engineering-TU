b03715812438731b36ddd9d18d9c49cd806d14bf
NikashDevX <lamsalnikash312@gmail.com>
2025-07-16 07:09:55 +0545

    dsa lab 6

---
+++ Semester IV/Data Structure & Algorithm/Lab/Lab-6/Bubble Sort .cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) { 
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; ++j) { 
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break; 
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 3, 7, 2, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ Semester IV/Data Structure & Algorithm/Lab/Lab-6/Linear Search .cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {5, 3, 7, 2, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target = 7;
+
+    int result = linearSearch(arr, n, target);
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found." << endl;
+    }
+
+    return 0;
+}
