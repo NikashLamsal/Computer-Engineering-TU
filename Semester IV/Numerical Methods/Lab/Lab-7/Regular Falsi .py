e9a2b32a1a34ac64f7febd8123ba81b130b8bdfc
NikashDevX <lamsalnikash312@gmail.com>
2025-07-25 08:03:04 +0545

    fixed isue

--- Semester IV/Numerical Methods/Lab/Lab-7/Regular Falsi .py
+++ Semester IV/Numerical Methods/Lab/Lab-7/Regular Falsi .py
@@ -1,31 +1,31 @@
 def f(x):
-    ans = x**3 - 3*x -2
-    return ans
+    return x**3 - 3*x - 2
 
 def regula_falsi(x1, x2, e):
     f1 = f(x1)
     f2 = f(x2)
-    x0 = x1
-    if f1*f2 > 0:
-        ans = "Enter valid value for x1 and x2"
-        return ans
 
-    while(True):
-        x0 = (x1*f(x2)-x2*f(x2))/(f(x2)-f(x1))
-       # x0 = x1 - f1 * (x2 - x1) / (f2 - f1)
+    if f1 * f2 > 0:
+        return "Enter valid values for x1 and x2 (they must bracket the root)"
 
+    while True:
+        # Modified formula version
+        x0 = (x1 * f2 - x2 * f1) / (f2 - f1)
         f0 = f(x0)
 
         if abs(f0) < e:
-            break        
+            break
 
-        if (f1*f0) < 0:
-            x2, f2 = x0, f0
+        if f1 * f0 < 0:
+            x2 = x0
+            f2 = f0
         else:
-            x1, f1 = x0, f0
-            
+            x1 = x0
+            f1 = f0
+
     return x0
 
-x1, x2, e = map(float, input("Enter x2, x2 and e:"))
+# Input
+x1, x2, e = map(float, input("Enter x1, x2 and e (error tolerance): ").split())
 root = regula_falsi(x1, x2, e)
-print("Required root: ",root)
+print("Required root:", root)
